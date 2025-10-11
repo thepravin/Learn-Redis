8b8116ee0f27b1219f7454903dde40536f56f093
Pravin Nalawade <thepravin28@gmail.com>
2025-10-11 13:01:02 +0530

    Redis cache added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+const client = require('./client');
 
 app.listen(9000, () => {
     console.log("Server running on : 9000");
@@ -8,7 +9,15 @@ app.listen(9000, () => {
 
 app.get('/', async (req, res) => {
     try {
+        const cacheValue = await client.get('todos');
+
+        if(cacheValue)return res.json(JSON.parse(cacheValue));
+
         const { data } = await axios.get('https://jsonplaceholder.typicode.com/todos');
+
+        await client.set("todos",JSON.stringify(data));
+        await client.expire("todos",30);
+
         return res.json(data);
     } catch (err) {
         console.error("Error fetching data:", err.message);
